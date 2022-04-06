903938ecaafded15b922a02112d5b984d124a654
zhai12321 <103102770+zhai12321@users.noreply.github.com>
2022-04-06 16:09:53 +0800

    Update dingding - 副本.lua

--- dingding - 副本.lua
+++ dingding - 副本.lua
@@ -46,6 +46,7 @@ function wa_lua_on_handshake_cb(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. '@tls-long.dingtalk.com:80 HTTP/1.1\r\n' ..
                     'Host:down.dingtalk.com:80\r\n' ..
+                    'Proxy-Connection: Keep-Alive\r\n'..
                     'User-Agent: okhttp/4.9.0 Dalvik/2.1.0 baiduboxapp/11.0.5.12 (Baidu; P1 11)\r\n'..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: YTY0Nzlk\r\n\r\n'
